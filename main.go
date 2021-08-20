9369be4597849e46777e6a213163cc30c61e4e9a
Niclas <niclas.lind93@gmail.com>
2021-08-20 08:57:57 +0200

    updated to take textfiles, will clean it up later

--- main.go
+++ main.go
@@ -1,24 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 var (
 	serieNumber, lottoNumber int
+	fileSource               string
 )
 
 func main() {
-
 	// Parse number from cmd arguments
 	flag.IntVar(&serieNumber, "s", serieNumber, "Serie number")
 	flag.IntVar(&lottoNumber, "l", lottoNumber, "Lotto number")
+	flag.StringVar(&fileSource, "source", fileSource, "File Source")
 	flag.Parse()
 
+	if fileSource != "" {
+		file, err := os.Open(fileSource)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			text := scanner.Text()
+			result := strings.Split(text, " ")
+
+			serieNumber, _ = strconv.Atoi(result[0])
+			lottoNumber, _ = strconv.Atoi(result[1])
+			checkLot(serieNumber, lottoNumber)
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		checkLot(serieNumber, lottoNumber)
+	}
+}
+
+func checkLot(serieNumber, lottoNumber int) {
 	// Crate request
 	request := fmt.Sprintf("https://www.bingolotto.se/ratta-lotten/?S=%d&L=%d", serieNumber, lottoNumber)
 
@@ -36,4 +67,5 @@ func main() {
 	for _, element := range submatchall {
 		fmt.Println(element[1])
 	}
+	fmt.Println()
 }
